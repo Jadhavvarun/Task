ccc59a56c4b8c91607e7625804320364be257fbb
Jadhavvarun <jadhavvarun67@gmail.com>
2024-06-09 21:59:25 +0530

    final design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +9,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, FormsModule ],
+  imports: [RouterOutlet, NavbarComponent, HeaderComponent, FooterComponent, FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
